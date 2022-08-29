61475962ad040ad6018838728a8c0e25d43a30c2
Jasper Alblas <just_jasper@hotmail.com>
2022-08-29 20:40:02 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('#container');
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+for (let index = 0; index < 898; index++) {
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon');
+
+    const label = document.createElement('span');
+    label.innerText = `#${index}`;
+
+    const newImage = document.createElement('img');
+    newImage.src = `${baseURL}${index + 1}.png`
+
+    pokemon.appendChild(newImage);
+    pokemon.appendChild(label);
+
+    container.appendChild(pokemon);
+}
